bcb1414683edb2073ec549e95437fc9c2f1cdd95
adunseayorinde <adunseayorinde@gmail.com>
2025-06-06 12:34:46 +0100

    removed redundant files

--- Models/QaModels.cs
+++ Models/QaModels.cs
@@ -9,9 +9,9 @@ public class QuestionRequest
 
 public class SourceMetadata
 {
-    public string Source { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public string Source { get; set; }
     public string Language { get; set; }
 }
 
