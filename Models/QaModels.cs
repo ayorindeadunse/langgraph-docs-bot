0b74b56f5983a33be26ae864689db02454971d5e
adunseayorinde <adunseayorinde@gmail.com>
2025-06-09 08:13:52 +0100

    fixed UI bugs

--- Models/QaModels.cs
+++ Models/QaModels.cs
@@ -7,16 +7,7 @@ public class QuestionRequest
     public string Question { get; set; } = string.Empty;
 }
 
-public class SourceMetadata
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Source { get; set; }
-    public string Language { get; set; }
-}
-
 public class QaResponse
 {
-    public string Answer { get; set; }
-    public List<SourceMetadata> Sources { get; set; } = new();
+    public string Response { get; set; } = string.Empty;
 }
